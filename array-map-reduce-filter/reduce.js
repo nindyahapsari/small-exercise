40d4eded6d0fdd35ad21266027f1b31b81f06358
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-01-16 14:17:10 +0100

    practice reduce from portal

--- array-map-reduce-filter/reduce.js
+++ array-map-reduce-filter/reduce.js
@@ -10,4 +10,28 @@ const averageCalories = menu.reduce((acc, curr) => {
   return acc + curr.calories;
 }, 0);
 
-console.log(averageCalories / menu.length);
+// console.log(averageCalories / menu.length);
+
+const product = {
+  name: "AmazonBasics Apple Certified Lightning to USB Cable",
+  price: 7.99,
+  manufacturer: "Amazon",
+  reviews: [
+    {
+      user: "Pavel Nedved",
+      comments: "It was really useful, strongly recommended",
+      rate: 4,
+    },
+    { user: "Alvaro Trezeguet", comments: "It lasted 2 days", rate: 1 },
+    { user: "David Recoba", comments: "Awesome", rate: 5 },
+    { user: "Jose Romero", comments: "Good value for money", rate: 4 },
+    { user: "Antonio Cano", comments: "It broked really fast", rate: 2 },
+  ],
+};
+
+const totalRate = product.reviews.reduce((acc, curr) => {
+  return acc + curr.rate;
+}, 0);
+
+const averageRate = totalRate / product.reviews.length;
+console.log(`total averageRate: ${averageRate}`);
